fa5e678ffca96920ed414ee058065918c8ebe123
Tomas Tulka <tomas.tulka@gmail.com>
2018-11-01 18:54:06 +0100

    tests groupped

--- money/exchange-lambda/test/exchangeTest.js
+++ money/exchange-lambda/test/exchangeTest.js
@@ -2,18 +2,21 @@ describe('exchange test suite', function() {
     
     const exchange = require('../src/exchange')
   
-    it('rate should be defined for EUR/USD exchange', async function() {
-        const rate = await exchange.rate('EUR', 'USD') 
+    it('rate test', async function() {
+        var rate
+        rate = await exchange.rate('EUR', 'USD') 
+        expect(rate).toBeDefined()
+        
+        rate = await exchange.rate('USD', 'EUR') 
         expect(rate).toBeDefined()
     });
     
-    it('rate should be defined for USD/EUR exchange', async function() {
-        const rate = await exchange.rate('USD', 'EUR') 
-        expect(rate).toBeDefined()
-    });
-    
-    it('rate should be 1 for the same currency', async function() {
-        const rate = await exchange.rate('USD', 'USD') 
+    it('rate identity test', async function() {
+        var rate
+        rate = await exchange.rate('USD', 'USD') 
+        expect(rate).toBe(1)
+        
+        rate = await exchange.rate('EUR', 'EUR') 
         expect(rate).toBe(1)
     });
 });
