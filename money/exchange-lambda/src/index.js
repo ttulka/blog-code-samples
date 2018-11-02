5e4e1993235c740e8b3cd534bf1c5f1adc484f8c
Tomas Tulka <tomas.tulka@gmail.com>
2018-11-02 10:04:16 +0100

    throwing errors added

--- money/exchange-lambda/src/index.js
+++ money/exchange-lambda/src/index.js
@@ -1,19 +1,15 @@
 const exchange = require('./exchange')
 
 exports.handler = async function(event) {
-    if (!event || !event.payload) {
-        return buildResponse(400, {error: 'Wrong request format.'})
+    if (!event.origin) {
+        throw new Error('"origin" must be set.')
     }
-
-    if (!event.payload.origin) {
-        return buildResponse(400, {error: '"origin" must be set.'})
-    }
-    if (!event.payload.target) {
-        return buildResponse(400, {error: '"target" must be set.'})
+    if (!event.target) {
+        throw new Error('"target" must be set.')
     }
     
     try {
-        const rate = await exchange.rate(event.payload.origin, event.payload.target)
+        const rate = await exchange.rate(event.origin, event.target)
     
         return {rate}
         
