5c55ae26f6ad9097af218d51d74d84694f8c927d
Tomas Tulka <tomas.tulka@gmail.com>
2018-11-02 10:04:39 +0100

    resources added

--- money/money-lambda/src/index.js
+++ money/money-lambda/src/index.js
@@ -6,14 +6,14 @@ const money = require('./money')
 const EXCHANGE_SERVICE = process.env.EXCHANGE_SERVICE
 
 exports.handler = async function(event) {
-    if (!event.resource || !event.queryStringParameters || event.httpMethod !== 'GET') {
+    if (!event.resource || !event.body || event.httpMethod !== 'POST') {
         return buildResponse(400, {error: 'Wrong request format.'})
     }
     
     try {
         switch (event.resource) {
-            case 'times': return await dispatchTimes(event.queryStringParameters) 
-            case 'reduce': return await dispatchReduce(event.queryStringParameters)
+            case '/times': return await dispatchTimes(JSON.parse(event.body)) 
+            case '/reduce': return await dispatchReduce(JSON.parse(event.body))
             default: buildResponse(400, {error: 'Unknown resource.'})
         }        
     } catch (e) {
@@ -43,22 +43,24 @@ async function dispatchReduce(params) {
         return buildResponse(400, {error: '"currency" query parameter must be set.'})
     }
 
-    const result = money.reduce(params.money, params.currency, exchange())
+    const result = await money.reduce(params.money, params.currency, exchange())
         
     return buildResponse(200, result)
 }
 
 function exchange() {
     return {
-        rate: (origin, target) => {
+        rate: async (origin, target) => {
             const params = {
                 FunctionName: EXCHANGE_SERVICE, 
-                Payload: {origin, target}
+                Payload: JSON.stringify({origin, target})
             }
             const res = await lambda.invoke(params).promise()
+            const payload = JSON.parse(res.Payload)
             
-            if (res.Payload.error) throw new Error(res.Payload.error)
-            return res.Payload.rate
+            if (res.FunctionError || payload.error) throw new Error(res.Payload)
+            
+            return payload.rate
         }    
     }
 }
