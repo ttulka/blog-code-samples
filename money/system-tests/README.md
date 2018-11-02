35181dedf7bf4536a6deb967aec8c0e5b0794eb8
Tomas Tulka <tomas.tulka@gmail.com>
2018-11-02 11:16:08 +0100

    system tests added

---
+++ money/system-tests/README.md
@@ -0,0 +1,12 @@
+# Money System Tests
+
+## Prerequisites
+```
+npm install -g jasmine
+``` 
+Environment variable `FACADE_URL` must be set.
+
+## Test
+```
+npm test
+```
---
+++ money/system-tests/test/moneyTestSys.js
@@ -0,0 +1,40 @@
+describe('money system test suite', function() {
+    
+    const axios = require('axios')
+    
+    const FACADE_URL = process.env.FACADE_URL
+  
+    it('multiplication test', async function() {
+        const params = {
+            money: {
+          		amount: 5,
+          		currency: 'USD'
+          	}, 
+          	multiplier: 2
+        }
+        const res = await axios.post(FACADE_URL + '/times', params)
+        
+        expect(res).toBeDefined()
+        expect(res.status).toBe(200)
+        
+        expect(res.data.amount).toBe(10)
+        expect(res.data.currency).toBe('USD')
+    })
+    
+    it('reduction test', async function() {
+        const params = {
+            money: {
+          		amount: 5,
+          		currency: 'USD'
+          	}, 
+          	currency: 'EUR'
+        }
+        const res = await axios.post(FACADE_URL + '/reduce', params)
+                
+        expect(res).toBeDefined()
+        expect(res.status).toBe(200)
+                
+        expect(res.data.amount).toBeDefined()
+        expect(res.data.currency).toBe('EUR')
+    })
+})
