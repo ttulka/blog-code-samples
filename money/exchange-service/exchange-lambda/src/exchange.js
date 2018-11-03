a60a19396ed0a125d4f9e3e95081aec153052e8a
Tomas Tulka <tomas.tulka@gmail.com>
2018-11-03 18:50:57 +0100

    todo added

--- money/exchange-service/exchange-lambda/src/exchange.js
+++ money/exchange-service/exchange-lambda/src/exchange.js
@@ -1,4 +1,9 @@
 exports.rate = async function(origin, target) {
     // TODO implement me
+    
+    // here could be a call to an external service like NYSE
+    // or providing data from our own database
+    // etc
+    
     return 1
 }
