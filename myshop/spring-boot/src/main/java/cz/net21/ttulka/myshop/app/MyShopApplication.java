cc29f7b9dd0a695afc5ff498e9f87233399c634e
Tomas Tulka <tomas.tulka@gmail.com>
2019-03-29 09:11:16 +0100

    base package

--- myshop/spring-boot/src/main/java/cz/net21/ttulka/myshop/app/MyShopApplication.java
+++ myshop/spring-boot/src/main/java/cz/net21/ttulka/myshop/app/MyShopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("cz.net21.ttulka.myshop.*")
+@ComponentScan("cz.net21.ttulka.myshop")
 public class MyShopApplication {
 
     public static void main(String[] args) {
