9ea53ee8477e8e4c564e412572204ea587b1fa63
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-03 18:29:10 +0200

    synchronized loaded

---
+++ shared-resources-among-threads/src/main/java/com/ttulka/samples/threading/BatchLoaderSync.java
@@ -0,0 +1,61 @@
+package com.ttulka.samples.threading;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+class BatchLoaderSync {
+
+    private final int batchSize;
+    private final JdbcTemplate jdbcTemplate;
+
+    private final Collection<String> resultsBatch = new ConcurrentSkipListSet<>();
+    private final AtomicInteger counter = new AtomicInteger();
+
+    private volatile boolean finished = false;
+
+    public void load(String result) {
+        resultsBatch.add(result);
+
+        if (finished || counter.incrementAndGet() >= batchSize) {
+            batchLoad();
+        }
+    }
+
+    public void finish() {
+        finished = true;
+        batchLoad();
+    }
+
+    synchronized private void batchLoad() {
+        List<String> batch = new ArrayList<>(resultsBatch);
+        resultsBatch.removeAll(batch);
+        counter.set(0);
+
+        jdbcTemplate.batchUpdate("INSERT INTO results (result) VALUES (?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, batch.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return batch.size();
+            }
+        });
+
+        // expensive work
+        new ExpensiveWorker().work();
+    }
+}
